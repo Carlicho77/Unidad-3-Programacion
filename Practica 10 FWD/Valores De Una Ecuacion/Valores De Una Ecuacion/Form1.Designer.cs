8d26ee0298cb792574dad9b687316bb35329a191
Carlicho77 <cnicanor547@gmail.com>
2025-04-04 21:08:44 -0600

    Add files via upload

---
+++ Practica 10 FWD/Valores De Una Ecuacion/Valores De Una Ecuacion/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Valores_De_Una_Ecuacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(475, 37);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Z= X    +   X";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(145, 176);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Numero";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(379, 176);
+            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(27, 25);
+            label3.TabIndex = 2;
+            label3.Text = "X";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(578, 176);
+            label4.Margin = new Padding(5, 0, 5, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(27, 25);
+            label4.TabIndex = 3;
+            label4.Text = "X";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(773, 176);
+            label5.Margin = new Padding(5, 0, 5, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(26, 25);
+            label5.TabIndex = 4;
+            label5.Text = "Z";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(103, 205);
+            listBox1.Margin = new Padding(5, 4, 5, 4);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(178, 229);
+            listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 25;
+            listBox2.Location = new Point(305, 205);
+            listBox2.Margin = new Padding(5, 4, 5, 4);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(178, 229);
+            listBox2.TabIndex = 6;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 25;
+            listBox3.Location = new Point(505, 205);
+            listBox3.Margin = new Padding(5, 4, 5, 4);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(178, 229);
+            listBox3.TabIndex = 7;
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 25;
+            listBox4.Location = new Point(708, 205);
+            listBox4.Margin = new Padding(5, 4, 5, 4);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(178, 229);
+            listBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Lime;
+            button1.Location = new Point(305, 466);
+            button1.Margin = new Padding(5, 4, 5, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(178, 36);
+            button1.TabIndex = 9;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 128);
+            button2.Location = new Point(505, 466);
+            button2.Margin = new Padding(5, 4, 5, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(178, 36);
+            button2.TabIndex = 10;
+            button2.Text = "Salir";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(531, 23);
+            label6.Name = "label6";
+            label6.Size = new Size(24, 25);
+            label6.TabIndex = 11;
+            label6.Text = "2";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(599, 23);
+            label7.Name = "label7";
+            label7.Size = new Size(24, 25);
+            label7.TabIndex = 12;
+            label7.Text = "3";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(401, 161);
+            label8.Name = "label8";
+            label8.Size = new Size(24, 25);
+            label8.TabIndex = 13;
+            label8.Text = "2";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(599, 161);
+            label9.Name = "label9";
+            label9.Size = new Size(24, 25);
+            label9.TabIndex = 14;
+            label9.Text = "3";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(220, 244, 254);
+            ClientSize = new Size(1042, 562);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox4);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 4, 5, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private Button button1;
+        private Button button2;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+    }
+}
