4f9729d88236e5022080505b42d86a51e377ba19
Carlicho77 <cnicanor547@gmail.com>
2025-04-04 20:44:33 -0600

    Add files via upload

---
+++ Practica 2/InvertirNumeros/InvertirNumeros/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace InvertirNumeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(73, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(166, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Ingrese un numero";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.Control;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(73, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(166, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Ingrese un numero";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.Control;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(73, 151);
+            label3.Name = "label3";
+            label3.Size = new Size(166, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Ingrese un numero";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.Control;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(73, 191);
+            label4.Name = "label4";
+            label4.Size = new Size(166, 23);
+            label4.TabIndex = 3;
+            label4.Text = "Ingrese un numero";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.FromArgb(255, 192, 192);
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(205, 376);
+            label5.Name = "label5";
+            label5.Size = new Size(34, 23);
+            label5.TabIndex = 4;
+            label5.Text = "N1";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(192, 255, 255);
+            textBox1.Location = new Point(319, 69);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(192, 255, 255);
+            textBox2.Location = new Point(319, 112);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(192, 255, 255);
+            textBox3.Location = new Point(319, 151);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(192, 255, 255);
+            textBox4.Location = new Point(319, 191);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.FromArgb(255, 192, 192);
+            label6.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(270, 376);
+            label6.Name = "label6";
+            label6.Size = new Size(34, 23);
+            label6.TabIndex = 9;
+            label6.Text = "N2";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.FromArgb(255, 192, 192);
+            label7.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(330, 376);
+            label7.Name = "label7";
+            label7.Size = new Size(34, 23);
+            label7.TabIndex = 10;
+            label7.Text = "N3";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.FromArgb(255, 192, 192);
+            label8.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(394, 376);
+            label8.Name = "label8";
+            label8.Size = new Size(34, 23);
+            label8.TabIndex = 11;
+            label8.Text = "N4";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Lime;
+            button1.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(634, 204);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 29);
+            button1.TabIndex = 12;
+            button1.Text = "Invertir ";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(634, 250);
+            button2.Name = "button2";
+            button2.Size = new Size(125, 29);
+            button2.TabIndex = 13;
+            button2.Text = "Salir";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Inverit Numeros";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Button button1;
+        private Button button2;
+    }
+}
