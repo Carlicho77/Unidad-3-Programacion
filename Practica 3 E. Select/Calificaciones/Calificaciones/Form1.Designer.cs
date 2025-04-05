8b4ce7d12ea3b78245114ddf90715a75e0152ea7
Carlicho77 <cnicanor547@gmail.com>
2025-04-04 21:21:28 -0600

    Add files via upload

---
+++ Practica 3 E. Select/Calificaciones/Calificaciones/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Calificaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(186, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(231, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Ingresa tu calificacion";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(490, 84);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 34);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 128);
+            button1.Location = new Point(186, 141);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Decision";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 128);
+            button2.Location = new Point(186, 197);
+            button2.Name = "button2";
+            button2.Size = new Size(107, 29);
+            button2.TabIndex = 3;
+            button2.Text = "Salir";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(934, 562);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 4, 5, 4);
+            Name = "Form1";
+            Text = "Calificaciones";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
